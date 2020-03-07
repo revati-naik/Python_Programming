8c54010eaa2b676e8f31e7e71ee66d76351b5a00
revati-naik <revatin@terpmail.umd.edu>
2020-03-07 11:45:58 -0500

    Code Commenting

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,7 @@
 
-
+#
+# Class TicTacToe to execute the logic for the game
+#
 class TicTacToe:
 
 	def __init__(self):
@@ -7,7 +9,9 @@ class TicTacToe:
 							"-", "-", "-",
 							"-", "-", "-"]
 
-
+	##
+	## Function to display the grid with the updated values from the user
+	##
 	def displayGrid(self):
 		print(" | %s  | %s | %s |" %(self.input_grid[1], self.input_grid[2], self.input_grid[3]))
 		print("---------------")
@@ -16,7 +20,17 @@ class TicTacToe:
 		print(" | %s  | %s | %s |" %(self.input_grid[7], self.input_grid[8], self.input_grid[9]))
 		print("---------------\n")
 
-		
+	##
+	## Updates the grid with trhe current input. Either "X" or "O"
+	##
+	## :param      grid_no:  The grid number to be updated with either "X" or "O"
+	## :type       grid_no:  int
+	## :param      user:     The user name "X" or "O" i.e 
+	## :type       user:     string
+	##
+	## :returns:   True if thye grid gets updated with the input. False otherwise
+	## :rtype:     boolean
+	##
 	def updateGrid(self,  grid_no, user):
 		if self.input_grid[grid_no] == "-":
 			self.input_grid[grid_no] = user
@@ -26,11 +40,26 @@ class TicTacToe:
 			print("Invalid Move...Choose another position")
 			return False
 
-
+	##
+	## Counts the number of empty positions available on the grid.
+	##
+	## :returns:   Number of empty positions on the grid.
+	## :rtype:     int
+	##
 	def countEmptyPositions(self):
 		return (self.input_grid.count("-"))
 
 
+	##
+	## Determines whether the specified user is winner. COnsists of 8 conditions which can cause 
+	## a player to win
+	##
+	## :param      user:  The user name. Either "X" or "O"
+	## :type       user:  string
+	##
+	## :returns:   True if the specified user is winner, False otherwise.
+	## :rtype:     boolean
+	##
 	def isWinner(self, user):
 		if self.input_grid[1] == user and self.input_grid[2] == user and self.input_grid[3] == user:
 			return True
@@ -56,6 +85,10 @@ class TicTacToe:
 		if self.input_grid[3] == user and self.input_grid[5] == user and self.input_grid[7] == user:
 			return True
 
+	
+	##
+	## Used to refresh the grid after every game. 
+	##
 	def refreshGrid(self):
 		self.input_grid = [None, "-", "-", "-",
 							"-", "-", "-",
--- main.py
+++ main.py
@@ -3,30 +3,57 @@ import numpy as np
 import cv2 
 import TicTacToe
 
-
+#
+# Header function for the game to dispaly the initial empty grid
+#
+# :type       tic_tac_toe:  class instance
+# :param      tic_tac_toe:  class instance passed to use the class method in the function
+#
 def startGame(tic_tac_toe):
-	print("\nTIC-TAC-TOE\n")
+	print("\nWelcome to TIC-TAC-TOE\n")
 	tic_tac_toe.displayGrid()
 
+#
+# Main function written to run the game which takes either user inputs or default inputs or unittest
+# inputs depending on where the function is called
+#
+# :type       test_case:  list
+# :param      test_case:  The test cases which is a list of multiple sublists for default test and
+#                         unittest. It is None for user inputs
+#
+# :returns:   Returns the result of the game. "True" for a win of the player (X or Y). Else "False".
+#             Or "draw" in case
+# :rtype:     string
+#
 def run_code(test_case):
 	result = None 
 	i = 0
+
+	# Loops until there are empty spaces available on the grid for more inputs
 	while tic_tac_toe.countEmptyPositions() > 0:
 		tic_tac_toe.displayGrid()
-		
-		# Get user input 1: X
+
+		# Get user input 1: "X"
 		success = False
 		while not success:
 			try:
+				# We use user inputs to test the game.
 				if test_case is None:
 					user_input_x = int(input("\nX: Choose 1-9: "))
+				
+				# We use the default test cases as user inputs
 				else:
 					user_input_x = test_case[i]
 			except Exception as e:
+				# The inputis not a number
 				print("Invalid input. Please try again.")
 				continue
+			# Checks if the user input or the default input lies between 0-9
 			if user_input_x > 0 and user_input_x < 10:
+				# Updates the input on the grid
 				success = tic_tac_toe.updateGrid(user_input_x, "X")
+				
+				# checks if the grid could add the new user input 
 				if not success:
 					i += 1
 					continue
@@ -43,25 +70,35 @@ def run_code(test_case):
 			print("X is the winner")
 			break
 
+		# Draw condition when all the spaces have been filled out on the grid
 		if tic_tac_toe.countEmptyPositions() == 0:
 			result = "draw"
 			print("DRAWWWW!!!")
 			break
 		i += 1
 
-		# Get user input 1: Y
+		# Get user input 2: "O"
 		success = False
 		while not success:
 			try:
+				# We use user inputs to test the game.
 				if test_case is None:
 					user_input_y = int(input("\nO: Choose 1-9: "))
+				
+				# We use the default test cases as user inputs
 				else:
 					user_input_y = test_case[i]
 			except Exception as e:
+				# The inputis not a number
 				print("Invalid input. Please try again.")
 				continue
+
+			# Checks if the user input or the default input lies between 0-9
 			if user_input_y > 0 and user_input_y < 10:
+				# Updates the input on the grid
 				success = tic_tac_toe.updateGrid(user_input_y, "O")
+
+				# checks if the grid could add the new user input 
 				if not success:
 					i += 1
 					continue
@@ -86,6 +123,7 @@ def run_code(test_case):
 tic_tac_toe = TicTacToe.TicTacToe()
 
 def main():
+	startGame(tic_tac_toe)
 	run_code(None)	
 	
 
